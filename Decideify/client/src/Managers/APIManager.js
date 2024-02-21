1f9893e5842ad52858a032736a92fed471b3099a
Colin Matteson <seefm@proton.me>
2024-02-21 15:46:24 -0500

    builds out an APIManager.js in React for handling fetch callsfrom the three different APIs we are using

---
+++ Decideify/client/src/Managers/APIManager.js
@@ -0,0 +1,52 @@
+const bookapikey = `vem6bjG9QLoaHhi1cJVquNnoojijS3r1`;
+const movietvapikey = `daa116e318f6510e3187f5a14dd286c4`;
+
+export const getallbooks = () => {
+    return fetch(`https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=${bookapikey}`)
+      .then((res) => res.json())
+  };
+
+export const getallmovies = () => {
+    const randomNumber = Math.floor(Math.random() * 500) + 1;
+
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYWExMTZlMzE4ZjY1MTBlMzE4N2Y1YTE0ZGQyODZjNCIsInN1YiI6IjY1YmJkN2U4MmI4YTQzMDE3YjFjMTkxMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fwxuyXsDPLTWtEDFMV1M5vKy_Y8WCyJrbRofqGr1do4'
+        }
+      };
+      
+      return fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${randomNumber}`, options)
+        .then(response => response.json())
+  };
+
+  export const getalltv = () => {
+    const randomNumber = Math.floor(Math.random() * 500) + 1;
+
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYWExMTZlMzE4ZjY1MTBlMzE4N2Y1YTE0ZGQyODZjNCIsInN1YiI6IjY1YmJkN2U4MmI4YTQzMDE3YjFjMTkxMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fwxuyXsDPLTWtEDFMV1M5vKy_Y8WCyJrbRofqGr1do4'
+        }
+      };
+      
+      return fetch(`https://api.themoviedb.org/3/tv/popular?language=en-US&page=${randomNumber}`, options)
+        .then(response => response.json())
+  };
+
+
+  export const getallmusic = () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        'User-Agent': 'Decideify/1.0 (colinfm88@gmail.com)',
+      },
+    };
+  
+    return fetch(`https://musicbrainz.org/ws/2/artist?query=ab&limit=100&offset=25&fmt=json`)
+      .then((res) => res.json());
+  };
+  
