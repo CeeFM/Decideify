8007009d5816a4158b5d3d2724b47f3ae2ed9e19
Colin Matteson <seefm@proton.me>
2024-02-20 19:16:34 -0500

    getting login and register pages working with matching authentication methods, login pages are loading but not connecting to the backend yet, still working

--- Decideify/obj/Debug/net8.0/Decideify.AssemblyInfo.cs
+++ Decideify/obj/Debug/net8.0/Decideify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Decideify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de8c02481ff21f31c752e5c487eb42612507a972")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Decideify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Decideify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
