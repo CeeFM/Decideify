30a0b3a14de5dad1b0a5ae202f9e2da5d53392a3
Colin Matteson <seefm@proton.me>
2024-02-27 20:37:39 -0500

    creates random date generator for NYT Books API fetch calls

--- Decideify/client/src/Components/Books.js
+++ Decideify/client/src/Components/Books.js
@@ -22,6 +22,34 @@ export default function Books() {
     ExternalId: "n/a"
   });
 
+  let start = new Date(2008, 5, 8)
+  let end = new Date()
+
+  const randomDate = () => {
+    let randomYear = Math.floor(Math.random() * (2024 - 2008 + 1)) + 2008;
+    let randomMonth = Math.floor(Math.random() * (12)) + 1;
+    let randomDay = Math.floor(Math.random() * (28)) + 1;
+    let newRandomDate = new Date(randomYear, randomMonth, randomDay);
+    let allGood = false;
+    while (allGood === false) {
+      if (start < newRandomDate && end > newRandomDate) {
+        allGood = true;
+      } else {
+        randomYear = Math.floor(Math.random() * (2024 - 2008 + 1)) + 2008;
+        randomMonth = Math.floor(Math.random() * (12)) + 1;
+        randomDay = Math.floor(Math.random() * (28)) + 1;
+        newRandomDate = new Date(randomYear, randomMonth, randomDay);
+      }
+    };
+
+    return newRandomDate.toISOString().split('T')[0];
+  }
+
+ const dateCreator = () => {
+   const testRandomDate = randomDate();
+   console.log(testRandomDate);
+ };
+
   const getbooks = () => {
     getallbooks().then((thesebooks) => setBookSuggestions(thesebooks));
   };
@@ -34,6 +62,8 @@ export default function Books() {
     randomList = Math.floor(Math.random() * bookSuggestions?.results?.lists?.length);
     randomBook = Math.floor(Math.random() * bookSuggestions?.results?.lists[randomList]?.books?.length);
     const filterTest = bookSuggestions?.results?.lists.filter((NYTList) => NYTList?.display_name.includes("Combined Print & E-Book Fiction"));
+    const dateTest = randomDate(start, end);
+    console.log(dateTest);
     console.log(filterTest);
     console.log(bookSuggestions?.results?.lists[randomList]?.books[randomBook]);
     thisSuggestion = bookSuggestions?.results?.lists[randomList]?.books[randomBook];
@@ -56,6 +86,7 @@ export default function Books() {
       <button onClick={getbooks} className="btn btn-secondary">Test The Book API</button>
       <button onClick={printbooks} className="btn btn-secondary">Print Books</button>
       <button onClick={saveSuggestion}>Save Book</button>
+      <button onClick={dateCreator}>Date Checker</button>
       </section>
     </>
 
