d6ef2e64721dea8c21cc00381cf2064741b08cb8
Colin Matteson <seefm@proton.me>
2024-03-03 15:26:05 -0500

    movies.js carousel is now showing five results at a time and allowing some cycling through, but not fully fixed yet so still working

--- Decideify/client/src/Components/ContentCarousel.js
+++ Decideify/client/src/Components/ContentCarousel.js
@@ -1,31 +1,37 @@
 import React, { useState } from "react";
-import { Carousel, CarouselControl, CarouselItem } from "reactstrap";
+import { Carousel, CarouselItem, CarouselControl } from "reactstrap";
 import Suggestion from "./Suggestion";
 
-export default function ContentCarousel ({ filteredSuggestions }) {
+export default function ContentCarousel({ filteredSuggestions }) {
   const [index, setIndex] = useState(0);
+  const itemsPerPage = 5;
 
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
+  const handleSelect = (selectedIndex, e) => setIndex(selectedIndex);
 
-  const next = () => {
-    setIndex((prevIndex) => (prevIndex === filteredSuggestions.length - 1 ? 0: prevIndex + 1));
-  };
+  const next = () => setIndex((prevIndex) => prevIndex + 1);
 
-  const previous = () => {
-    setIndex((prevIndex) => (prevIndex === 0 ? filteredSuggestions.length - 1 : prevIndex - 1));
-  };
+  const previous = () => setIndex((prevIndex) => prevIndex - 1);
 
   return (
-    <Carousel interval={null} activeIndex={index} onSelect={handleSelect} next={next} previous={previous} className="container">
-      {filteredSuggestions.map((suggestion) => (
-        <CarouselItem key={suggestion.id}>
-          <Suggestion userSugg={suggestion} />
-        </CarouselItem>
-      ))}
+    <Carousel interval={null} activeIndex={index} onSelect={handleSelect} className="container">
+      {Array.from(
+        { length: Math.ceil(filteredSuggestions.length / itemsPerPage) },
+        (_, pageIndex) => (
+          <CarouselItem key={pageIndex}>
+            <div className="row">
+              {filteredSuggestions
+                .slice(pageIndex * itemsPerPage, (pageIndex + 1) * itemsPerPage)
+                .map((suggestion) => (
+                  <div className="col" key={suggestion.id}>
+                    <Suggestion userSugg={suggestion} />
+                  </div>
+                ))}
+            </div>
+          </CarouselItem>
+        )
+      )}
       <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
       <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
     </Carousel>
   );
-};
+}
