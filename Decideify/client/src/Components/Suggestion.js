7820b343c6cd9a4b3983d5830bf95e2000a318a6
Colin Matteson <seefm@proton.me>
2024-03-04 13:26:01 -0500

    added a X icon to delete a suggestion, need to wire it up to the backend and make that possible, but I am also playing with some werid stuff so I wanted to store these changes before I start getting wild

--- Decideify/client/src/Components/Suggestion.js
+++ Decideify/client/src/Components/Suggestion.js
@@ -1,10 +1,14 @@
 import React from "react";
 import yes from "../Images/YES.png"
 import no from "../Images/NO.png"
+import x from "../Images/X.png"
 
 export default function Suggestion({ userSugg }) {
   return (
     <>
+            <div className="text-right">
+            <img style={{width: "2rem"}} src={x} alt="a big neon red x that you can use as a delete button for saved suggestions"/>
+            </div>
             <div className="text-center">
             <img src={userSugg?.imageLocation} style={{width: "10rem"}} alt={userSugg?.title} />
             <p>{userSugg?.title}</p>
