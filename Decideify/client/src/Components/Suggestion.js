c7a93074e096f53be64e41a062ad7ee5a49897dc
Colin Matteson <seefm@proton.me>
2024-03-04 13:45:08 -0500

    addresses carousel next and previous button issues by applying various styling changes, including replacing the native controls with custom buttons that do the same thing

--- Decideify/client/src/Components/Suggestion.js
+++ Decideify/client/src/Components/Suggestion.js
@@ -7,14 +7,14 @@ export default function Suggestion({ userSugg }) {
   return (
     <>
             <div className="text-right">
-            <img style={{width: "2rem"}} src={x} alt="a big neon red x that you can use as a delete button for saved suggestions"/>
+            <button style={{padding: "none", borderRadius: "5rem"}}><img style={{width: "1.5rem"}} src={x} alt="a big neon red x that you can use as a delete button for saved suggestions"/></button>
             </div>
             <div className="text-center">
             <img src={userSugg?.imageLocation} style={{width: "10rem"}} alt={userSugg?.title} />
             <p>{userSugg?.title}</p>
             <div>Recommend?</div>
-            <button><img src={yes} alt="yes" style={{width: "4rem"}}/></button>
-            <button><img src={no} alt="yes" style={{width: "3.75rem"}}/></button>
+            <button><img src={yes} alt="yes" style={{width: "3rem"}}/></button>
+            <button><img src={no} alt="yes" style={{width: "2.75rem"}}/></button>
             </div>
     </>
 
