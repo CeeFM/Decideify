1d66fa723a4c05fbb09e7039e8eba2981444dcce
Colin Matteson <seefm@proton.me>
2024-03-06 11:05:26 -0500

    fixes some initial tag suggestion problems, now posts are set up to conditionally render content based on whether or not there is a post tag attached to the post

--- Decideify/client/src/Components/Social/Post.js
+++ Decideify/client/src/Components/Social/Post.js
@@ -1,17 +1,41 @@
 import React from "react";
 import { Card, CardBody, CardImg } from "reactstrap";
-import { Link } from "react-router-dom";
 import { useState, useEffect } from 'react';
-import { getAllPostTags, getPostTagByPostId } from "../../Managers/PostTagManager";
+import { getAllPostTags } from "../../Managers/PostTagManager";
 
 export const Post = ({ thisPost }) => {
+
+    const [allTags, setAllTags] = useState([]);
+    const [thisTag, setThisTag] = useState();
+
+    const getAllTags = () => {
+        getAllPostTags().then((theseTags) => {
+            setAllTags(theseTags);
+            let isThereATagHere = theseTags.find((tag) => tag?.postId === thisPost?.id);
+            setThisTag(isThereATagHere);
+        });
+    };
+
+    const tagTesting = () => {
+        console.log(thisPost);
+        console.log(allTags);
+        console.log(thisTag);
+    }
+
+    useEffect(() => {
+        getAllTags();         
+    }, [])
 
     return (
     <>
       <div className="container d-flex justify-content-center">
         <Card className="m-4" >
           <CardBody>
+            { thisTag === undefined ?
               <CardImg top src={thisPost?.imageLocation} style={{ width: '30rem' }} />
+              :
+              <CardImg top src={thisTag?.suggestion?.imageLocation} style={{ width: '30rem' }} />
+            }
             <p>
             </p>
             <h3 style={{color: "#ff00bb"}}>{thisPost.title}</h3>
@@ -19,6 +43,7 @@ export const Post = ({ thisPost }) => {
             <p>
                 Written by: {thisPost?.userProfile?.username}
             </p>
+            <button className="btn btn-primary" onClick={tagTesting}>Tag Testing</button>
           </CardBody>
         </Card>
       </div>
