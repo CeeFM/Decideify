c7b82af9141b821f434749a46ef9217107461d81
Colin Matteson <seefm@proton.me>
2024-03-17 22:16:57 -0400

    fixes edit comment, so now edit and delete comment are working for any comments the logged in user has authored

--- Decideify/client/src/Components/Social/Comment.js
+++ Decideify/client/src/Components/Social/Comment.js
@@ -7,6 +7,44 @@ export const Comment = ({ comment, setComments, thisPost }) => {
 
   const localUserProfile = localStorage.getItem("userProfile");
   const decideifyUserObject = JSON.parse(localUserProfile);
+ 
+  const [showForm, setShowForm] = useState(false)
+  const [editComment, setEditComment] = useState({})
+
+  useEffect(() => {
+    setEditComment(comment);
+  }, [])
+
+  const handleControlledInputChange = (e) => {
+
+    const newComment = { ...editComment }
+
+    newComment[`${e.target.name}`] = e.target.value
+
+    setEditComment(newComment)
+  }
+
+  const UpdateEntry = (e) => {
+    e.preventDefault()
+
+    const entryToSend = { ...editComment}
+
+
+    fetch(`https://localhost:5001/api/comment/${comment.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(entryToSend),
+    })      
+    .then(() => {
+      return getCommentsByPostId(thisPost.id)
+    })
+    .then((theseComments) => {
+      setComments(theseComments);
+      setShowForm(!showForm);
+    })
+  } 
 
   const deleteThisComment = () => {
     deleteComment(comment.id)
@@ -22,6 +60,7 @@ export const Comment = ({ comment, setComments, thisPost }) => {
 
     return (
         <>
+        { showForm === false ? (
         <div className="container" style={{border: "gray solid 1px", color: "#4cf7e6", width: "100%", height:"auto", borderRadius: "2rem", paddingTop: "2rem", marginTop: "1.5rem", paddingBottom: "1.5rem", fontFamily: "Bebas Neue"}}>
         
         <h4>{comment?.subject}</h4>
@@ -35,15 +74,31 @@ export const Comment = ({ comment, setComments, thisPost }) => {
   <img src={comment?.userProfile?.imageLocation} alt="the post author's picture" style={{ width: '3rem', borderRadius: '8rem', marginBottom: "1rem" }}
   />
 </div>
-      {comment?.userProfileId === decideifyUserObject.id ?
+{comment?.userProfileId === decideifyUserObject.id ?
       <>
-        <button className="btn btn-warning">Edit Comment</button> <button className="btn btn-danger" onClick={deleteThisComment}>Delete Comment</button>
+        <button className="btn btn-warning" onClick={() => setShowForm(!showForm)}>Edit Comment</button> <button className="btn btn-danger" onClick={deleteThisComment}>Delete Comment</button>
       </>
       :
       ""
       }
+</div>
+) : (
+<div className="container d-flex justify-content-center">
+      <Card className="w-100" >
+        <CardBody>
+          <p>Comment Subject: </p>
+          <input style={{fontSize: "1.25rem", fontFamily: "Bebas Neue", marginBottom: ".5vh"}} name="subject" type="text" placeholder="" value={editComment.subject} onChange={handleControlledInputChange} />
+          <p>Comment Content: </p>
+          <input style={{fontSize: "1.25rem", fontFamily: "Bebas Neue", marginBottom: ".5vh"}} name="content" type="text" placeholder="" value={editComment.content} onChange={handleControlledInputChange} />
+        </CardBody>
+        <div>
+        <button className="btn btn-success" onClick={(e) => UpdateEntry(e)}> Save </button>
+        <button className="btn btn-danger" onClick={() => setShowForm(!showForm)}> Cancel </button>
         </div>
+      </Card>
+    </div>
+)}
         </>
-    )
+    );
 
-}
+};
